24daa87bfc04ebde43300c6b62c2bead94fef9d9
Danylo <danil.bogomazov1356@gmail.com>
2026-02-08 16:25:36 +0200

    Add kwargs check

--- app/main.py
+++ app/main.py
@@ -1,16 +1,19 @@
-from typing import Callable
+from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
     story = {}
 
-    def wrapper(*args, **kwargs) -> Callable:
-        if args in story:
+    def wrapper(*args, **kwargs) -> Any:
+        if args in story or tuple(kwargs.values()) in story:
             print("Getting from cache")
         else:
-            story[args] = func(*args, **kwargs)
+            if len(args) != 0:
+                story[args] = func(*args, **kwargs)
+            else:
+                story[tuple(kwargs.values())] = func(*args, **kwargs)
             print("Calculating new result")
-        return story[args]
+        return story[args] if len(args) != 0 else story[tuple(kwargs.values())]
     return wrapper
 
 
